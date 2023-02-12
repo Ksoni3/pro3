5728c0b05867b73c2999856067a03436cd117f46
Kamal Soni <iamkamalsoni7@gmail.com>
2023-02-12 15:23:41 +1100

    Setup of API and ContextAPI done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
+import { AppContext } from "./context/contextApi";
+
 
 function App() {
-  return (
+  return <AppContext>
     <div className="App">
-      
+     <h1 className="text-xs"> Kamal </h1>
     </div>
-  );
+  </AppContext>
+    
 }
 
 export default App;
---
+++ src/context/contextApi.js
@@ -0,0 +1,46 @@
+import { createContext } from "react";
+import React, {useState, useEffect} from 'react';
+
+import { fetchDataFromApi } from "../utils/api";
+
+export const Context = createContext();
+
+
+
+export const AppContext = (props) => {
+    const [loading, setLoading] = useState(false);
+    const [searchResults, setSearchResults] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("New");
+    const [mobileMenu, setMobileMenu] = useState(false);
+
+    useEffect(() => {
+        fetchSelectedCategoryData(selectedCategory);
+    }, [selectedCategory]);
+
+    const fetchSelectedCategoryData = (query) => {
+        setLoading(true);
+        fetchDataFromApi(`search/?q=${query}`)
+        .then((res) => {
+        console.log(res.contents);
+        // setSearchResults(contents);
+        setLoading(false);
+        });
+    };
+
+    return (
+        <Context.Provider
+            value={{
+                loading,
+                setLoading,
+                searchResults,
+                selectedCategory,
+                setSelectedCategory,
+                mobileMenu,
+                setMobileMenu,
+            }}
+        >
+            {props.children}
+        </Context.Provider>
+    );
+};
+
